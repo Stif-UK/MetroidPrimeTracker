c4408fc54046d78a23041d60ed4f2d29874efabe
Grant Hutchison <stif@grants-mbp.lan>
2023-02-17 14:43:46 +0000

    setup buttons and progress indicator ui

--- lib/ui/MetroidHome.dart
+++ lib/ui/MetroidHome.dart
@@ -19,33 +19,198 @@ class _MetroidHomeState extends State<MetroidHome> {
       ),
       body: Column(
         children: [
-          CheckboxListTile(
-            title: const Text("Gravity Chamber"),
-            subtitle: const Text("Energy Tank"),
-            secondary: const Icon(Icons.electric_bolt),
-            controlAffinity: ListTileControlAffinity.leading,
-            value: _checked,
-            onChanged: (bool? value){
-              setState(() {
-                _checked = value!;
-              });
-            }
+          const SizedBox(height: 20),
 
-          ),
-          CheckboxListTile(
-              title: const Text("Life Grove"),
-              subtitle: const Text("Missile Expansion"),
-              secondary: const Icon(Icons.ac_unit),
-              controlAffinity: ListTileControlAffinity.leading,
-              value: _checked2,
-              onChanged: (bool? value){
-                setState(() {
-                  _checked = value!;
-                });
-              }
+          //Button 1: Tallon Overworld
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+                onPressed: (){},
+                //onPressed: (){ Get.to(() => WearStats());},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: const BorderSide(color: Colors.black)
+                        )
 
+                    )
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text("Tallon Overworld",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                )
+            ),
           ),
+          Row(
+            children: [
+              Expanded(
+                  flex:1,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: LinearProgressIndicator(value: 0.5,),
+          )),
+              Text("50%")
+            ],
+          ),
+          const Divider(thickness: 4,),
+
+          //Button 2: Chozo Ruins
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+                onPressed: (){},
+                //onPressed: (){ Get.to(() => WearStats());},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: const BorderSide(color: Colors.black)
+                        )
+
+                    )
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text("Chozo Ruins",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                )
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LinearProgressIndicator(value: 0.3,),
+                  )),
+              Text("30%")
+            ],
+          ),
+          const Divider(thickness: 4,),
+
+          //Button 3: Magmoor Caverns
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+                onPressed: (){},
+                //onPressed: (){ Get.to(() => WearStats());},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: const BorderSide(color: Colors.black)
+                        )
+
+                    )
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text("Magmoor Caverns",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                )
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LinearProgressIndicator(value: 0.2,),
+                  )),
+              Text("20%")
+            ],
+          ),
+          const Divider(thickness: 4,),
+
+
+      //Button 4: Phendrana Drifts
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+                onPressed: (){},
+                //onPressed: (){ Get.to(() => WearStats());},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: const BorderSide(color: Colors.black)
+                        )
+
+                    )
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text("Phendrana Drifts",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                )
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LinearProgressIndicator(value: 0.6,),
+                  )),
+              Text("60%")
+            ],
+          ),
+          const Divider(thickness: 4,),
+
+
+          //Button 5: Phazon Mines
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+                onPressed: (){},
+                //onPressed: (){ Get.to(() => WearStats());},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: const BorderSide(color: Colors.black)
+                        )
+
+                    )
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Text("Phazon Mines",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                )
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LinearProgressIndicator(value: 0.9,),
+                  )),
+              Text("90%")
+            ],
+          ),
+          const Divider(thickness: 4,),
+
+
         ],
+
       ),
     );
   }
