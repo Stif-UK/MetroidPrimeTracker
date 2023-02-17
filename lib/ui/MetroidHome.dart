0a07abeb8c1d2dd792c715a9b7c2caffb11827cf
Grant Hutchison <stif@grants-mbp.lan>
2023-02-17 14:08:47 +0000

    quick prototype

--- lib/ui/MetroidHome.dart
+++ lib/ui/MetroidHome.dart
@@ -8,11 +8,44 @@ class MetroidHome extends StatefulWidget {
 }
 
 class _MetroidHomeState extends State<MetroidHome> {
+  var _checked = false;
+  var _checked2 = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Metroid Prime Item Tracker"),
+      ),
+      body: Column(
+        children: [
+          CheckboxListTile(
+            title: const Text("Gravity Chamber"),
+            subtitle: const Text("Energy Tank"),
+            secondary: const Icon(Icons.electric_bolt),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: _checked,
+            onChanged: (bool? value){
+              setState(() {
+                _checked = value!;
+              });
+            }
+
+          ),
+          CheckboxListTile(
+              title: const Text("Life Grove"),
+              subtitle: const Text("Missile Expansion"),
+              secondary: const Icon(Icons.ac_unit),
+              controlAffinity: ListTileControlAffinity.leading,
+              value: _checked2,
+              onChanged: (bool? value){
+                setState(() {
+                  _checked = value!;
+                });
+              }
+
+          ),
+        ],
       ),
     );
   }
