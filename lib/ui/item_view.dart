691607c476a0b7a825dd84c29465107ff3487192
Grant Hutchison <stif@grants-mbp.lan>
2023-02-21 11:16:49 +0000

    update build number and fix overflow issue on item screen

--- lib/ui/item_view.dart
+++ lib/ui/item_view.dart
@@ -31,75 +31,77 @@ class _ItemViewState extends State<ItemView> {
       appBar: AppBar(
         title: Text(widget.currentItem.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            title: Text("Region: ${ItemHelper.getRegionNameFromString(widget.currentItem.region)}"),
-          ),
-          const Divider(thickness: 2,),
-          ListTile(
-            title: Text("Room: ${widget.currentItem.room}"),
-          ),
-          const Divider(thickness: 2,),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              title: Text("Region: ${ItemHelper.getRegionNameFromString(widget.currentItem.region)}"),
+            ),
+            const Divider(thickness: 2,),
+            ListTile(
+              title: Text("Room: ${widget.currentItem.room}"),
+            ),
+            const Divider(thickness: 2,),
 
-          SwitchListTile(
-            title: const Text("Collected:"),
-              value: widget.currentItem.collected,
-              onChanged: (value){
-              setState(() {
-                widget.currentItem.collected = value;
-                widget.currentItem.save();
-              });
-              }),
-          const Divider(thickness: 2,),
+            SwitchListTile(
+              title: const Text("Collected:"),
+                value: widget.currentItem.collected,
+                onChanged: (value){
+                setState(() {
+                  widget.currentItem.collected = value;
+                  widget.currentItem.save();
+                });
+                }),
+            const Divider(thickness: 2,),
 
-          const ListTile(
-            title: Text("Notes:"),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Form(
-              key: _editKey,
-              child: TextFormField(
-                initialValue: widget.currentItem.description,
-                enabled: true,
-                maxLines: 7,
-                onSaved: (String? value){
-                  value != null? _notes = value : _notes = "";
-                } ,
-                decoration: InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Theme.of(context).disabledColor,
-                        )
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.lightBlue
-                        )
-                    )
+            const ListTile(
+              title: Text("Notes:"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _editKey,
+                child: TextFormField(
+                  initialValue: widget.currentItem.description,
+                  enabled: true,
+                  maxLines: 7,
+                  onSaved: (String? value){
+                    value != null? _notes = value : _notes = "";
+                  } ,
+                  decoration: InputDecoration(
+                      disabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Theme.of(context).disabledColor,
+                          )
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.lightBlue
+                          )
+                      )
+                  ),
                 ),
               ),
             ),
-          ),
-          Center(
-            child: ElevatedButton(
-                onPressed: (){
-                  _editKey.currentState?.save();
-                  widget.currentItem.description = _notes;
-                  widget.currentItem.save();
-                  Get.snackbar(
-                    widget.currentItem.title, 'Note Saved',
-                    icon: ItemHelper.getItemTypeIcon(widget.currentItem.type,),
-                    snackPosition: SnackPosition.BOTTOM
-                  );
-                },
-                child: const Text("Save Note"),),
-          )
-        ],
+            Center(
+              child: ElevatedButton(
+                  onPressed: (){
+                    _editKey.currentState?.save();
+                    widget.currentItem.description = _notes;
+                    widget.currentItem.save();
+                    Get.snackbar(
+                      widget.currentItem.title, 'Note Saved',
+                      icon: ItemHelper.getItemTypeIcon(widget.currentItem.type,),
+                      snackPosition: SnackPosition.BOTTOM
+                    );
+                  },
+                  child: const Text("Save Note"),),
+            )
+          ],
 
+        ),
       )
     );
   }
