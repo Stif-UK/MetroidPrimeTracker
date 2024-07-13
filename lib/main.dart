24a03d91684499cafd2df0f14ea821c5357485de
Grant Hutchison <stif@grants-mbp.lan>
2024-07-13 09:38:04 +0100

    updating target SDK and implementing new theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:metroid_prime_items/api/purchase_api.dart';
 import 'package:metroid_prime_items/model/itemsmodel.dart';
 import 'package:metroid_prime_items/model/metroid_preferences.dart';
-import 'package:metroid_prime_items/theme/theme_constants.dart';
 import 'package:metroid_prime_items/ui/MetroidHome.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:metroid_prime_items/ui/first_use_demo/onboarding.dart';
 import 'package:metroid_prime_items/ui/initialise_screen.dart';
 import 'package:provider/provider.dart';
 import 'provider/adstate.dart';
+import 'package:json_theme/json_theme.dart';
 
 import 'helper/item_generator.dart';
 
@@ -30,6 +32,14 @@ void main() async {
   //Get SharedPreferences
   await MetroidPreferences.init();
 
+  //Load the theme from assets
+  final themeStrDark = await rootBundle.loadString('assets/theme/mp_dark_theme.json');
+  final themeJsonDark = jsonDecode(themeStrDark);
+  final themeDark = ThemeDecoder.decodeThemeData(themeJsonDark);
+
+  final themeStrLight = await rootBundle.loadString('assets/theme/mp_theme.json');
+  final themeJsonLight = jsonDecode(themeStrLight);
+  final themeLight = ThemeDecoder.decodeThemeData(themeJsonLight);
 
   //TODO: Move box population into loading widget
   if(Hive.box<Items>("ItemBox").isEmpty){
@@ -56,8 +66,8 @@ void main() async {
       debugShowCheckedModeBanner: false,
       title: 'MP Checklist',
 
-      theme: lightTheme ,
-      darkTheme: darkTheme,
+      theme: themeLight ,
+      darkTheme: themeDark,
       themeMode: ThemeMode.system,
 
 
